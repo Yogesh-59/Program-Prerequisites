e78a07dff7223ea96050e2569a881bd350e18e75
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-01-08 10:49:27 +0530

    Perimeter of a Rectangle is Done

---
+++ PerimeterofaRectangle.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+import java.util.Scanner;
+
+public class PerimeterofaRectangle {
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+         int length=sc.nextInt();
+          int width=sc.nextInt();
+         int Perimeter=(lenght+width)*2;
+         // find the Perimeter of a Rectangle of given problem
+          System.out.println("Perimeter of a Rectangle is"+Perimeter);
+         
+    }
+}
