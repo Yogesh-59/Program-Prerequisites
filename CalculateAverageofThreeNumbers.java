ce8e2670122735cdb155110b13d8c874679d7417
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-01-08 11:11:01 +0530

    CalculateAverageofThreeNumbers

---
+++ CalculateAverageofThreeNumbers.java
@@ -0,0 +1,18 @@
+import java.util.*; 
+import java.util.Scanner;
+
+public class CalculateAverageofThreeNumbers{
+     
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+         double num1=sc.nextInt();
+         double num2=sc.nextInt();
+         double num3=sc.nextInt();
+         double Average= (num1+num2+num3)/3;
+        System.out.println("The average of the three numbers is: " + Average);
+
+
+         
+    }
+}
