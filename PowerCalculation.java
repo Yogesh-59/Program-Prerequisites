57ac18d0cb2451fd6c758b59827a89abd86e742c
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-01-08 11:04:58 +0530

    Power Calculation problem is Done

---
+++ PowerCalculation.java
@@ -0,0 +1,25 @@
+import java.util.*; 
+import java.util.Scanner;
+
+public class PowerCalculation {
+     publis static int power(int base, int exponent){
+         if (exponent ==0){
+           return 1;
+          }
+         else{
+           return base*powe(base,exponent-1);
+          }
+ }
+
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+         int base=sc.nextInt();
+          int exponent=sc.nextInt();
+          
+         int result = power(base, exponent);
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+
+         
+    }
+}
