21d57895bf8a601ebad7bd2b956fdca243208cda
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-01-08 10:20:39 +0530

    Add Two Numbers program Done

---
+++ AddToNumber.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AddToNumber{
+ public static void main(String[]args){
+   Scanner sc=new Scanner(System.in);
+   int num1=sc.nextInt();
+   int num2=sc.nextInt();
+   int ans=num1+num2;
+   System.out.println("Add to numbers"+ans);
+}
+}
